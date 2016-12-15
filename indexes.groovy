56d8ecda5df2c99746d0a46fb6f4073393a26ca6
Eugene Prokopiev <enp@itx.ru>
2016-12-15 15:51:22 +0300

    initial commit - show all indexes

---
+++ indexes.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+
+@Grab('io.vertx:vertx-core:[3,)')
+@Grab('io.vertx:vertx-lang-groovy:[3,)')
+@Grab('io.vertx:vertx-web:[3,)')
+@Grab('io.vertx:vertx-web-templ-handlebars:[3,)')
+
+def vertx  = io.vertx.groovy.core.Vertx.vertx()
+def router = io.vertx.groovy.ext.web.Router.router(vertx)
+def engine = io.vertx.groovy.ext.web.templ.HandlebarsTemplateEngine.create()
+
+router.get().handler({ context ->
+  vertx.createHttpClient().get(9200, '127.0.0.1','/_aliases', { response ->
+    response.bodyHandler({ body ->
+      context.put("indexes", body.toJsonObject())
+      engine.render(context, "indexes", { result ->
+        if (result.succeeded()) {
+          context.response().end(result.result())
+        } else {
+          context.fail(result.cause())
+        }
+      })
+    })
+    response.exceptionHandler({ error ->
+      context.fail(error)
+    })
+  }).end()
+})
+
+vertx.createHttpServer().requestHandler(router.&accept).listen(8200)
